1e0ca60df263d685ce54d24beeb5479fa7b62a2d
Mateus Davi Simon <mateusdsimon@gmail.com>
2022-07-21 13:56:40 -0300

    Inital commit: unification.

---
+++ src/term.rs
@@ -0,0 +1,138 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Term {
+    Functor { name: String, arguments: Vec<Term> },
+    Variable { name: String },
+}
+
+impl Term {
+    pub fn variable<S: ToString>(name: S) -> Self {
+        Self::Variable {
+            name: name.to_string(),
+        }
+    }
+
+    pub fn atom<S: ToString>(name: S) -> Self {
+        Self::Functor {
+            name: name.to_string(),
+            arguments: vec![],
+        }
+    }
+
+    pub fn functor<S: ToString, I: IntoIterator<Item = Term>>(name: S, arguments: I) -> Self {
+        let arguments = arguments.into_iter().collect();
+        Self::Functor {
+            name: name.to_string(),
+            arguments,
+        }
+    }
+
+    pub fn substitute(self, var_name: &str, term: &Term) -> Self {
+        match self {
+            Self::Variable { name } if name == var_name => term.clone(),
+            Self::Functor { arguments, name } => Self::Functor {
+                name,
+                arguments: arguments
+                    .into_iter()
+                    .map(|arg| arg.substitute(var_name, term))
+                    .collect(),
+            },
+            _ => self,
+        }
+    }
+}
+
+impl Display for Term {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Variable { name } => {
+                write!(f, "{}", name)?;
+            }
+            Self::Functor { name, arguments } => {
+                write!(f, "{}", name)?;
+                if !arguments.is_empty() {
+                    write!(f, "(")?;
+                    for i in 0..arguments.len() {
+                        write!(f, "{}", arguments[i])?;
+                        if i < arguments.len() - 1 {
+                            write!(f, ", ")?;
+                        }
+                    }
+                    write!(f, ")")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn build_term() {
+        let term = Term::functor("father", [Term::atom("a"), Term::variable("X")]);
+
+        use Term::*;
+        assert_eq!(
+            term,
+            Functor {
+                name: String::from("father"),
+                arguments: vec![
+                    Functor {
+                        name: String::from("a"),
+                        arguments: vec![]
+                    },
+                    Variable {
+                        name: String::from("X")
+                    },
+                ]
+            }
+        )
+    }
+
+    #[test]
+    fn substitute_variable() {
+        let original_term = Term::variable("X");
+
+        let subistituded = original_term.substitute("X", &Term::atom("a"));
+
+        assert_eq!(subistituded, Term::atom("a"));
+    }
+
+    #[test]
+    fn do_not_substitute_different_variable() {
+        let original_term = Term::variable("Y");
+
+        let subistituded = original_term.substitute("X", &Term::atom("a"));
+
+        assert_eq!(subistituded, Term::variable("Y"));
+    }
+
+    #[test]
+    fn do_not_substitute_atom() {
+        let original_term = Term::atom("b");
+
+        let subistituded = original_term.substitute("X", &Term::atom("a"));
+
+        assert_eq!(subistituded, Term::atom("b"));
+    }
+
+    #[test]
+    fn substitute_fuctor_arguments() {
+        let original_term = Term::functor(
+            "test",
+            [Term::atom("b"), Term::variable("X"), Term::variable("Y")],
+        );
+
+        let subistituded = original_term.substitute("X", &Term::atom("a"));
+
+        let expected_term = Term::functor(
+            "test",
+            [Term::atom("b"), Term::atom("a"), Term::variable("Y")],
+        );
+        assert_eq!(subistituded, expected_term)
+    }
+}
---
+++ src/unification.rs
@@ -0,0 +1,351 @@
+use super::term::Term;
+
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Assignments {
+    assignments: HashMap<String, Term>,
+}
+
+impl Assignments {
+    pub fn empty() -> Self {
+        Self {
+            assignments: HashMap::new(),
+        }
+    }
+
+    pub fn add<S: ToString>(&mut self, variable: S, value: Term) {
+        self.assignments.insert(variable.to_string(), value);
+    }
+
+    pub fn with<S: ToString>(mut self, variable: S, value: Term) -> Self {
+        self.assignments.insert(variable.to_string(), value);
+        self
+    }
+
+    pub fn merge(mut self, other: Self) -> Option<Self> {
+        for (key, value) in other.assignments.into_iter() {
+            if let Some(old_value) = self.assignments.get(&key) {
+                if *old_value != value {
+                    return None;
+                }
+            } else {
+                self.assignments.insert(key, value);
+            }
+        }
+        Some(self)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum UnifyResult {
+    True(Assignments),
+    False,
+}
+
+pub fn unify(a: Term, b: Term) -> UnifyResult {
+    unify_terms(vec![(a, b)])
+}
+
+fn unify_terms(mut constraints: Vec<(Term, Term)>) -> UnifyResult {
+    if let Some((lhs, rhs)) = constraints.pop() {
+        if lhs == rhs {
+            unify_terms(constraints)
+        } else {
+            match (lhs, rhs) {
+                (Term::Variable { name }, term) => {
+                    let subistituded_constraints = constraints
+                        .into_iter()
+                        .map(|(ta, tb)| (ta.substitute(&name, &term), tb.substitute(&name, &term)))
+                        .collect();
+                    if let UnifyResult::True(assignments) = unify_terms(subistituded_constraints) {
+                        UnifyResult::True(assignments.with(name, term))
+                    } else {
+                        UnifyResult::False
+                    }
+                }
+                (term, Term::Variable { name }) => {
+                    let subistituded_constraints = constraints
+                        .into_iter()
+                        .map(|(ta, tb)| (ta.substitute(&name, &term), tb.substitute(&name, &term)))
+                        .collect();
+                    if let UnifyResult::True(assignments) = unify_terms(subistituded_constraints) {
+                        UnifyResult::True(assignments.with(name, term))
+                    } else {
+                        UnifyResult::False
+                    }
+                }
+                (
+                    Term::Functor {
+                        name: a_name,
+                        arguments: a_args,
+                    },
+                    Term::Functor {
+                        name: b_name,
+                        arguments: b_args,
+                    },
+                ) if a_name == b_name && a_args.len() == b_args.len() => {
+                    constraints
+                        .append(&mut a_args.into_iter().zip(b_args.into_iter()).rev().collect());
+                    unify_terms(constraints)
+                }
+                _ => UnifyResult::False,
+            }
+        }
+    } else {
+        UnifyResult::True(Assignments::empty())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+
+    #[test]
+    fn unify_equal_atoms() {
+        let term_a = Term::atom("a");
+        let term_b = Term::atom("a");
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(Assignments::empty())
+        );
+    }
+
+    #[test]
+    fn do_not_unify_different_atoms() {
+        let term_a = Term::atom("a");
+        let term_b = Term::atom("b");
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False);
+    }
+
+    #[test_case(Term::atom("a"))]
+    #[test_case(Term::variable("Y"))]
+    #[test_case(Term::functor("test", [Term::variable("Z"), Term::atom("k")]))]
+    fn unify_variable_left_with_anything(something: Term) {
+        let var = Term::variable("X");
+
+        let expected_assignments = Assignments::empty().with("X", something.clone());
+        assert_eq!(
+            unify(var, something),
+            UnifyResult::True(expected_assignments)
+        );
+    }
+
+    #[test_case(Term::atom("a"))]
+    #[test_case(Term::functor("test", [Term::variable("Z"), Term::atom("k")]))]
+    fn unify_variable_right_with_anything(something: Term) {
+        let var = Term::variable("X");
+
+        let expected_assignments = Assignments::empty().with("X", something.clone());
+        assert_eq!(
+            unify(something, var),
+            UnifyResult::True(expected_assignments)
+        );
+    }
+
+    #[test]
+    fn unify_functors() {
+        let term_a = Term::functor("test", [Term::atom("a"), Term::atom("b")]);
+        let term_b = Term::functor("test", [Term::atom("a"), Term::atom("b")]);
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(Assignments::empty())
+        )
+    }
+
+    #[test]
+    fn do_not_unify_functors_with_different_names() {
+        let term_a = Term::functor("test", [Term::atom("a"), Term::atom("b")]);
+        let term_b = Term::functor("atest", [Term::atom("a"), Term::atom("b")]);
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False,)
+    }
+
+    #[test]
+    fn do_not_unify_functors_with_different_arities() {
+        let term_a = Term::functor("test", [Term::atom("a"), Term::atom("b")]);
+        let term_b = Term::functor("test", [Term::atom("a"), Term::atom("b"), Term::atom("c")]);
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False,)
+    }
+
+    #[test]
+    fn unify_functors_with_variables() {
+        let term_a = Term::functor("test", [Term::variable("X"), Term::atom("b")]);
+        let term_b = Term::functor("test", [Term::atom("a"), Term::variable("Y")]);
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(
+                Assignments::empty()
+                    .with("X", Term::atom("a"))
+                    .with("Y", Term::atom("b"))
+            )
+        )
+    }
+
+    #[test]
+    fn do_not_unify_functors_with_variables_with_incompatible_assignments() {
+        let term_a = Term::functor("test", [Term::variable("X"), Term::atom("b")]);
+        let term_b = Term::functor("test", [Term::atom("a"), Term::variable("X")]);
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False)
+    }
+
+    #[test]
+    fn unify_complex_term() {
+        let term_a = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::variable("X"), Term::variable("Y")]),
+                    Term::functor("point", [Term::variable("X"), Term::variable("Z")]),
+                ],
+            )],
+        );
+
+        let term_b = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::atom("1"), Term::atom("1")]),
+                    Term::functor("point", [Term::atom("1"), Term::atom("3")]),
+                ],
+            )],
+        );
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(
+                Assignments::empty()
+                    .with("X", Term::atom("1"))
+                    .with("Y", Term::atom("1"))
+                    .with("Z", Term::atom("3"))
+            )
+        )
+    }
+
+    #[test]
+    fn do_not_unify_complex_term() {
+        let term_a = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::variable("X"), Term::variable("Y")]),
+                    Term::functor("point", [Term::variable("X"), Term::variable("Z")]),
+                ],
+            )],
+        );
+
+        let term_b = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::atom("1"), Term::atom("1")]),
+                    Term::functor("point", [Term::atom("2"), Term::atom("3")]),
+                ],
+            )],
+        );
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False)
+    }
+
+    #[test]
+    fn unify_complex_term_with_variables_on_both_sides() {
+        let term_a = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::variable("X"), Term::variable("Y")]),
+                    Term::functor("point", [Term::variable("X"), Term::variable("Z")]),
+                ],
+            )],
+        );
+
+        let term_b = Term::functor(
+            "vertical",
+            [Term::functor(
+                "line",
+                [
+                    Term::functor("point", [Term::atom("1"), Term::variable("K")]),
+                    Term::functor("point", [Term::atom("1"), Term::atom("3")]),
+                ],
+            )],
+        );
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(
+                Assignments::empty()
+                    .with("X", Term::atom("1"))
+                    .with("Y", Term::variable("K"))
+                    .with("Z", Term::atom("3"))
+            )
+        )
+    }
+
+    #[test]
+    fn unify_terms_with_transitional_non_conflicting_assignments() {
+        let term_a = Term::functor(
+            "test",
+            [
+                Term::variable("X"),
+                Term::variable("X"),
+                Term::variable("Y"),
+            ],
+        );
+
+        let term_b = Term::functor(
+            "test",
+            [Term::variable("Y"), Term::atom("a"), Term::atom("a")],
+        );
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(
+                Assignments::empty()
+                    .with("X", Term::variable("Y"))
+                    .with("Y", Term::atom("a"))
+            )
+        )
+    }
+
+    #[test]
+    fn do_not_unify_terms_with_transitional_conflicting_assignments() {
+        let term_a = Term::functor(
+            "test",
+            [
+                Term::variable("X"),
+                Term::variable("X"),
+                Term::variable("Y"),
+            ],
+        );
+
+        let term_b = Term::functor(
+            "test",
+            [Term::variable("Y"), Term::atom("a"), Term::atom("b")],
+        );
+
+        assert_eq!(unify(term_a, term_b), UnifyResult::False)
+    }
+
+    #[test]
+    fn process_argumens_in_given_order() {
+        let term_a = Term::functor("test", [Term::variable("X"), Term::variable("Y")]);
+        let term_b = Term::functor("test", [Term::variable("Y"), Term::variable("X")]);
+
+        assert_eq!(
+            unify(term_a, term_b),
+            UnifyResult::True(Assignments::empty().with("X", Term::variable("Y")))
+        )
+    }
+}
